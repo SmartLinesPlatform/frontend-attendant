5be06cb40198f619b8cb942ff3b803f94eefa7da
Vitor Souza <vhsouzaeng@gmail.com>
2021-05-20 21:24:14 -0300

    tela de login

--- src/pages/Login/login.tsx
+++ src/pages/Login/login.tsx
@@ -8,44 +8,26 @@ import sline from '../../assets/sline.png';
 
 const Login: React.FC = () => {
   return (
-    <div className="logincontainer">
+    <div className="loginContainer" >
+      <img src={logo} alt="smartlines" className="loginlogo" />
 
-      <div className="logincontainer" >
-      <img src={logo} alt="smartlines" className="loginlogo"/>
-        <a className="links" id="paracadastro"></a>
-        <a className="links" id="paralogin"></a>
-        
-        <div className="content">
-          <div id="login">
-            <form method="post" action="">
-            
-              <p>
-                <label htmlFor="nome_login">Seu email:</label>
-                <input id="nome_login" name="nome_login" type="text" placeholder="ex. contato@htmlecsspro.com" />
-              </p>
-
-              <p>
-                <label htmlFor="email_login">Sua senha:</label>
-                <input id="email_login" name="email_login" type="password" placeholder="ex. senha" />
-              </p>
-
-              <p>
-                <input type="checkbox" name="manterlogado" id="manterlogado" value="" />
-                <label htmlFor="manterlogado">Manter-me logado</label>
-              </p>
-
-              <p>
-              <a href="/index"><button type="button" className="btn-1">Logar</button></a>
-              </p>
-
-              <p className="link">
-                Ainda não tem conta?
-              <a href="/cadastro">Cadastre-se</a>
-              </p>
-            </form>
-          </div>
+      <form method="post" action="">
+        <div className="loginInput">
+          <label htmlFor="nome_login">Seu email:</label>
+          <input id="nome_login" name="nome_login" type="text" placeholder="ex. contato@htmlecsspro.com" />
         </div>
-      </div>
+
+        <div className="loginInput">
+          <label htmlFor="email_login">Sua senha:</label>
+          <input id="email_login" name="email_login" type="password" placeholder="ex. senha" />
+        </div>
+
+        <div className="loginCheckbox">
+          <input type="checkbox" name="manterlogado" id="manterlogado" value="" />
+          <label htmlFor="manterlogado">Manter-me logado</label>
+        </div>
+        <button type="button" className="btnLogin">Logar</button>
+      </form>
     </div>
   );
 }
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import {Route, BrowserRouter} from 'react-router-dom';
+import { Route, BrowserRouter } from 'react-router-dom';
 import Cadastro from './pages/Cadastro/cadastro';
 import Home from './pages/Home/index';
 import Login from './pages/Login/login';
@@ -8,9 +8,9 @@ import Login from './pages/Login/login';
 const Routes = () => {
     return (
         <BrowserRouter>
-            <Route component = {Home} path = "/index" />
-            <Route component = {Login} path = "/" exact/>
-            <Route component = {Cadastro} path = "/cadastro"/>        
+            <Route component={Home} path="/index" />
+            <Route component={Login} path="/" exact />
+            <Route component={Cadastro} path="/cadastro" />
         </BrowserRouter>
     );
 }
