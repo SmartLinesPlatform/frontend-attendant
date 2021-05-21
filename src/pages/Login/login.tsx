a39e8cc4bfad15888e1e778d06dd9565d275b8ef
Vitor Souza <vhsouzaeng@gmail.com>
2021-05-20 22:36:51 -0300

    ajeitando o link da pagina

--- src/pages/Login/login.tsx
+++ src/pages/Login/login.tsx
@@ -5,8 +5,10 @@ import './styles.css';
 import logo from '../../assets/logo.svg';
 import arrow from '../../assets/arrow.svg';
 import sline from '../../assets/sline.png';
+import { useHistory } from 'react-router-dom';
 
 const Login: React.FC = () => {
+  const history = useHistory();
   return (
     <div className="loginContainer" >
       <img src={logo} alt="smartlines" className="loginlogo" />
@@ -26,7 +28,7 @@ const Login: React.FC = () => {
           <input type="checkbox" name="manterlogado" id="manterlogado" value="" />
           <label htmlFor="manterlogado">Manter-me logado</label>
         </div>
-        <button type="button" className="btnLogin">Logar</button>
+        <button type="button" className="btnLogin" onClick={() => history.push('/index')}>Logar</button>
       </form>
     </div>
   );
